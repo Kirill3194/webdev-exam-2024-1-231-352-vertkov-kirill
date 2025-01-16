0a65d389406f91a35f3d4011f471a4d385029301
Кирилл Вертков <82602566+Kirill3194@users.noreply.github.com>
2025-01-17 01:56:14 +0300

    Полностью сверстана главная страница, работает поиск курсов, поиск репетиторов и пагинация репетиторов и курсов

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
-getTutors().then(tutors => {
+let allTutors = [];
+
+function showTutors(page) {
     const parent = document.getElementById('tutor-column');
-    for (const tutor of tutors) {
-        parent.insertAdjacentHTML('beforeend', `<div class="col-md-4">
-          <div class="card">
-            <p style="margin-left: 10px;">Tutor ${tutor.id} - Name: ${tutor.name}  - Qualifications: ${tutor.languages_offered.join(', ')} - 
-            Experience: ${tutor.work_experience} years - Language level: ${tutor.language_level}</p>
-          </div>
-        </div>`)
-    }
-})
+
+    getTutors().then(tutors => {
+        allTutors = tutors;
+        parent.innerHTML = '';
+        tutors = tutors.slice(page * 3, Math.min((page + 1) * 3), tutors.length);
+        for (const tutor of tutors) {
+            parent.insertAdjacentHTML('beforeend', `<div class="col-md-4">
+            <div class="card">
+                <p style="margin-left: 10px;">Tutor ${tutor.id} - Name: ${tutor.name} - Qualifications: ${tutor.languages_offered.join(', ')} - 
+                Experience: ${tutor.work_experience} years - Language level: ${tutor.language_level}</p>
+            </div>
+            </div>`)
+        }
+    })
+}
 
 let allCourses = [];
+
 function showCourses(page) {
     const parent = document.getElementById('course-column');
     
@@ -29,8 +38,48 @@ function showCourses(page) {
     })
 }
 
-let page = 1;
-showCourses(page);
+function searchCourses(name, level) {
+    const parent = document.getElementById('search-courses-column');
+    
+    getCourses().then(courses => {
+        allCourses = courses;
+        parent.innerHTML = '';
+        for (const course of courses) {
+            if ((name == '' || name.toLowerCase() == course.name.toLowerCase()) && level == course.level.toLowerCase()) {
+                parent.insertAdjacentHTML('beforeend', `<div class="col-md-4">
+                    <div class="card">
+                        <p style="margin-left: 10px;">${course.id} - Name: ${course.name} - Level: ${course.level} - Teacher: ${course.teacher} - Week length: ${course.total_length}</p>
+                    </div>
+                </div>`)
+            }
+        }
+    })
+}
+
+function searchTutors(qualifications, experience) {
+    const parent = document.getElementById('tutor-search-column');
+
+    getTutors().then(tutors => {
+        parent.innerHTML = '';
+        for (const tutor of tutors) {
+            if ((qualifications.toLowerCase() == tutor.language_level.toLowerCase() || qualifications == 'all') && (experience == 'all' || 
+                (experience == '1' && 0 <= tutor.work_experience && 3 >= tutor.work_experience) || (experience == '2' && 3 <= tutor.work_experience && 5 >= tutor.work_experience) ||
+                (experience == '3' && 5 <= tutor.work_experience))) {
+                parent.insertAdjacentHTML('beforeend', `<div class="col-md-4">
+                <div class="card">
+                    <p style="margin-left: 10px;">Tutor ${tutor.id} - Name: ${tutor.name} - Qualifications: ${tutor.languages_offered.join(', ')} - 
+                    Experience: ${tutor.work_experience} years - Language level: ${tutor.language_level}</p>
+                </div>
+                </div>`)
+                }
+        }
+    })
+}
+
+let page1 = 0;
+let page2 = 0
+showCourses(page1);
+showTutors(page2)
 
 document.addEventListener("DOMContentLoaded", function() {
     // Получаем кнопку, которая открывает модальное окно
@@ -41,12 +90,72 @@ document.addEventListener("DOMContentLoaded", function() {
     const closeModalButton2 = document.getElementById('close-modal2');
     const closeModalButton3 = document.getElementById('close-modal1');
     const closeModalButton4 = document.querySelector('.close1');
+    const previousCourses = document.getElementById('previous_courses');
+    const nextCourses = document.getElementById('next_courses');
+    const previousTutors = document.getElementById('previous_tutors');
+    const nextTutors = document.getElementById('next_tutors');
     const userAccountButton = document.querySelector('#user-account'); // Кнопка для открытия модального окна
     const userAccountModal = new bootstrap.Modal(document.getElementById('userAccountModal')); // Получаем модальное окно
+    const formSearchCourses = document.querySelector('#search-courses form');
+    const inputSearchCoursesName = formSearchCourses.querySelector('input[name="course_name"]');
+    const selectSearchCoursesLevel = formSearchCourses.querySelector('select[name="level"]');
+    const formTutorSearch = document.querySelector('#tutor-search form');
+    const selectTutorSearchQualifications = formTutorSearch.querySelector('.form-control3');
+    const selectTutorSearchExperience = formTutorSearch.querySelector('.form-control4');
+    
 
     // Открываем модальное окно при нажатии на кнопку
     openModalButton.addEventListener('click', function() {
         modal.show();
+    });
+
+    formTutorSearch.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const qualifications = selectTutorSearchQualifications.value;
+        const experience = selectTutorSearchExperience.value;
+        searchTutors(qualifications, experience)
+    });
+
+    formSearchCourses.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const courseName = inputSearchCoursesName.value;
+        const level = selectSearchCoursesLevel.value;
+        
+        searchCourses(courseName, level);
+    });
+
+    previousCourses.addEventListener('click', function() {
+        page1 -= 1
+        if (page1 < 0){
+            page1 = 0
+        }
+        showCourses(page1);
+    });
+
+    nextCourses.addEventListener('click', function() {
+        page1 += 1
+        if (page1 > (Math.floor((allCourses.length + 2) / 3) - 1)){
+            page1 = (Math.floor((allCourses.length + 2) / 3) - 1)
+        }
+        showCourses(page1);
+    });
+
+    previousTutors.addEventListener('click', function() {
+        page2 -= 1
+        if (page2 < 0){
+            page2 = 0
+        }
+        showTutors(page2);
+    });
+
+    nextTutors.addEventListener('click', function() {
+        page2 += 1
+        if (page2 > (Math.floor((allTutors.length + 2) / 3) - 1)){
+            page2 = (Math.floor((allTutors.length + 2) / 3) - 1)
+        }
+        showTutors(page2);
     });
 
     // Закрываем модальное окно при нажатии на кнопку закрытия
